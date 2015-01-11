debbf676b169478620625b5cfce20e2bff38c202
sgrvinod <sv2414@columbia.edu>
2015-01-11 13:37:12 -0800

    All required files for course project 1.

---
+++ plot2.R
@@ -0,0 +1,36 @@
+#Check for and load required packages
+if(!require(plyr)) install.packages("plyr")
+library(plyr)
+if(!require(graphics)) install.packages("graphics")
+library(graphics)
+if(!require(data.table)) install.packages("data.table")
+library(data.table)
+if(!require(grDevices)) install.packages("grDevices")
+library(grDevices)
+
+#Check for and create folder to store data
+if (!file.exists("Data"))
+  dir.create("Data")
+
+#Check if data has already been downloaded to above folder
+#If not, download data
+if (!file.exists("Data/household_power_consumption.zip")){
+  #Define URL to download data from
+  url<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+  #Download data to folder
+  #This may take some time since size of data is ~ 20MB
+  download.file(url, "Data/household_power_consumption.zip")
+}
+
+#Since the downloaded data is in a zip file, first extract it and then import only required rows
+power_cons<-read.table(unz("Data/household_power_consumption.zip", "household_power_consumption.txt"),sep=";",skip=66637,nrows=2880,na.strings="?",col.names=c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+
+#Create new column for datetime using date and time columns, convert to POSIXlt
+power_cons$DateTime=paste(power_cons$Date,power_cons$Time)
+power_cons$DateTime<-strptime(power_cons$DateTime,format="%d/%m/%Y %H:%M:%S")
+
+#Plot plot2 and save to file
+png(filename="plot2.png",width=480,height=480,units="px")
+with(power_cons,plot(DateTime,Global_active_power,ylab="Global Active Power (kilowatts)",xlab="",type="n"))
+with(power_cons,lines(DateTime,Global_active_power))
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,50 @@
+#Check for and load required packages
+if(!require(plyr)) install.packages("plyr")
+library(plyr)
+if(!require(graphics)) install.packages("graphics")
+library(graphics)
+if(!require(data.table)) install.packages("data.table")
+library(data.table)
+if(!require(grDevices)) install.packages("grDevices")
+library(grDevices)
+
+#Check for and create folder to store data
+if (!file.exists("Data"))
+  dir.create("Data")
+
+#Check if data has already been downloaded to above folder
+#If not, download data
+if (!file.exists("Data/household_power_consumption.zip")){
+  #Define URL to download data from
+  url<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+  #Download data to folder
+  #This may take some time since size of data is ~ 20MB
+  download.file(url, "Data/household_power_consumption.zip")
+}
+
+#Since the downloaded data is in a zip file, first extract it and then import only required rows
+power_cons<-read.table(unz("Data/household_power_consumption.zip", "household_power_consumption.txt"),sep=";",skip=66637,nrows=2880,na.strings="?",col.names=c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+
+#Create new column for datetime using date and time columns, convert to POSIXlt
+power_cons$DateTime=paste(power_cons$Date,power_cons$Time)
+power_cons$DateTime<-strptime(power_cons$DateTime,format="%d/%m/%Y %H:%M:%S")
+
+#Plot plot4 and save to file
+png(filename="plot4.png",width=480,height=480,units="px")
+par(mfrow=c(2,2))
+#1/4
+with(power_cons, plot(DateTime,Global_active_power,type="n",xlab="",ylab="Global Active Power"))
+with(power_cons,lines(DateTime,Global_active_power))
+#2/4
+with(power_cons,plot(DateTime,Voltage,type="n",xlab="datetime",ylab="Voltage"))
+with(power_cons,lines(DateTime,Voltage))
+#3/4
+with(power_cons,plot(DateTime,Sub_metering_1,ylab="Energy sub metering",xlab="",type="n"))
+with(power_cons,lines(DateTime,Sub_metering_1))
+with(power_cons,lines(DateTime,Sub_metering_2,col="red"))
+with(power_cons,lines(DateTime,Sub_metering_3,col="blue"))
+legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),col=c("black","red","blue"),lty=1,bty="n")
+#4/4
+with(power_cons,plot(DateTime,Global_reactive_power,type="n",xlab="datetime",ylab="Global_reactive_power"))
+with(power_cons,lines(DateTime,Global_reactive_power))
+dev.off()
